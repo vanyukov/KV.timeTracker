55445838515405554d77c3e2f8a4648fa886a90b
Ванюков Кирилл <kirill.vanyukov@lenvendo.ru>
2021-05-20 17:49:57 +0300

    prettier

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,34 +1,36 @@
-import React, { useState } from 'react';
-import { Button, Container, NavDropdown, Row, Col } from 'react-bootstrap';
-import withStore from '~/hocs/withStore';
-import { getDatePresentation } from '~/api/helpers/dateTime';
-import ItemEditModal from '~/containers/ItemEditModal';
-import tracks from '~/api/db/tracks';
-import { Link, withRouter } from 'react-router-dom';
-import Style from './Header.module.css';
-import WeekNav from '~/containers/Header/WeekNav';
-import * as navigation from '~/api/helpers/navigation';
+import React, { useState } from 'react'
+import { Button, Container, NavDropdown, Row, Col } from 'react-bootstrap'
+import withStore from '~/hocs/withStore'
+import { getDatePresentation } from '~/api/helpers/dateTime'
+import ItemEditModal from '~/containers/ItemEditModal'
+import tracks from '~/api/db/tracks'
+import { Link, withRouter } from 'react-router-dom'
+import Style from './Header.module.css'
+import WeekNav from '~/containers/Header/WeekNav'
+import * as navigation from '~/api/helpers/navigation'
 
 function Header(props) {
-  const [trackEdit, setTrackEdit] = useState(tracks.getNew());
-  const [showPopup, setShowPopup] = useState(false);
+  const [trackEdit, setTrackEdit] = useState(tracks.getNew())
+  const [showPopup, setShowPopup] = useState(false)
   const createNewTrack = () => {
-    props.stores.TracksStore.fillNewTrack(tracks.getNew()).then(trackFilled => {
-      props.stores.TracksStore.start(trackFilled);
-      setTrackEdit(trackFilled);
-      setShowPopup(true);
-    });
-  };
+    props.stores.TracksStore.fillNewTrack(tracks.getNew()).then(
+      (trackFilled) => {
+        props.stores.TracksStore.start(trackFilled)
+        setTrackEdit(trackFilled)
+        setShowPopup(true)
+      }
+    )
+  }
 
   return (
     <Container>
       <Row>
         <Col>
           <header className={'d-flex align-items-center'}>
-            <Link to='/' className={'mr-2'}>
-              <img src='/img/logo.png' className={Style.logo_img} />
+            <Link to="/" className={'mr-2'}>
+              <img src="/img/logo.png" className={Style.logo_img} />
             </Link>
-            <Button variant='success' className='mr-2' onClick={createNewTrack}>
+            <Button variant="success" className="mr-2" onClick={createNewTrack}>
               New +
             </Button>
 
@@ -36,24 +38,24 @@ function Header(props) {
               navigation.isYearInUrl() ? window.location.pathname : undefined
             )}
 
-            <NavDropdown title='menu' id='header-nav-dropdown'>
+            <NavDropdown title="menu" id="header-nav-dropdown">
               <NavDropdown.Item as={'div'}>
-                <Link to='/settings'>Settings</Link>
+                <Link to="/settings">Settings</Link>
               </NavDropdown.Item>
               <NavDropdown.Item as={'div'}>
-                <Link to='/reports'>Reports</Link>
+                <Link to="/reports">Reports</Link>
               </NavDropdown.Item>
               <NavDropdown.Item as={'div'}>
-                <Link to='/comments'>Сохраненные комментарии</Link>
+                <Link to="/comments">Сохраненные комментарии</Link>
               </NavDropdown.Item>
               <NavDropdown.Item as={'div'}>
-                <Link to='/clients'>Клиенты</Link>
+                <Link to="/clients">Клиенты</Link>
               </NavDropdown.Item>
               <NavDropdown.Item as={'div'}>
-                <Link to='/utzJobTypes'>Типы работ УТЗ</Link>
+                <Link to="/utzJobTypes">Типы работ УТЗ</Link>
               </NavDropdown.Item>
               <NavDropdown.Item as={'div'}>
-                <Link to='/db'>Операции с DB</Link>
+                <Link to="/db">Операции с DB</Link>
               </NavDropdown.Item>
             </NavDropdown>
           </header>
@@ -69,7 +71,7 @@ function Header(props) {
         setTrackEdit={setTrackEdit}
       />
     </Container>
-  );
+  )
 }
 
-export default withRouter(withStore(Header));
+export default withRouter(withStore(Header))
