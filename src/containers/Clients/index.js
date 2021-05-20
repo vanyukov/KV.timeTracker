06f23d896be219ba79a036aac6b7b7b25c07cbd4
Ванюков Кирилл <kirill.vanyukov@lenvendo.ru>
2021-05-20 16:56:26 +0300

    prettier

--- src/containers/Clients/index.js
+++ src/containers/Clients/index.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import withStore from '~/hocs/withStore';
-import { Container, Row, Col, Button, Form } from 'react-bootstrap';
-import SelectTypeUtz from '~/components/selectTypeUtz';
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import withStore from '~/hocs/withStore'
+import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 
 function Clients(props) {
-  const history = useHistory();
+  const history = useHistory()
   const goBack = () => {
-    history.goBack();
-  };
+    history.goBack()
+  }
 
-  const loadDefault = () => props.stores.Clients.loadDefault();
+  const loadDefault = () => props.stores.Clients.loadDefault()
 
   return (
     <Container>
       <h1 className={'mt-2'}>
         Клиенты
         <Button
-          variant='outline-secondary'
-          size='sm'
+          variant="outline-secondary"
+          size="sm"
           className={'ml-3'}
           onClick={goBack}
         >
@@ -28,72 +27,72 @@ function Clients(props) {
       <Form className={'mb-2'}>
         <Row>
           <Col className={'mb-2'}>
-            <Form.Control value={'Название'} type='text' disabled={true} />
+            <Form.Control value={'Название'} type="text" disabled={true} />
           </Col>
           <Col sm={2}>
-            <Form.Control value={'id'} type='text' disabled={true} />
+            <Form.Control value={'id'} type="text" disabled={true} />
           </Col>
           <Col sm={2}>
-            <Form.Control value={'taskCode'} type='text' disabled={true} />
+            <Form.Control value={'taskCode'} type="text" disabled={true} />
           </Col>
           <Col sm={2}>
-            <Form.Control value={'taskIdCr'} type='text' disabled={true} />
+            <Form.Control value={'taskIdCr'} type="text" disabled={true} />
           </Col>
           <Col sm={3}> </Col>
         </Row>
-        {props.stores.Clients.items.map(item => {
+        {props.stores.Clients.items.map((item) => {
           return (
-            <Form.Group as={Row} controlId='formHorizontalEmail' key={item.key}>
+            <Form.Group as={Row} controlId="formHorizontalEmail" key={item.key}>
               <Col>
                 <Form.Control
                   value={item.title}
                   as={'textarea'}
                   onChange={() => {}}
-                  type='text'
+                  type="text"
                 />
               </Col>
               <Col sm={2}>
-                <Form.Control value={item.id} onChange={() => {}} type='text' />
+                <Form.Control value={item.id} onChange={() => {}} type="text" />
               </Col>
               <Col sm={2}>
                 <Form.Control
                   value={item.taskCode}
                   onChange={() => {}}
-                  type='text'
+                  type="text"
                 />
               </Col>
               <Col sm={2}>
                 <Form.Control
                   value={item.taskIdCr}
                   onChange={() => {}}
-                  type='text'
+                  type="text"
                 />
               </Col>
               <Col sm={3}>
                 <Button
-                  variant='primary'
-                  size='sm'
+                  variant="primary"
+                  size="sm"
                   className={'mr-2'}
                   onClick={() => props.stores.Clients.saveClient(item)}
                 >
                   Save
                 </Button>
                 <Button
-                  variant='outline-danger'
-                  size='sm'
+                  variant="outline-danger"
+                  size="sm"
                   onClick={() => props.stores.Clients.deleteClient(item)}
                 >
                   del
                 </Button>
               </Col>
             </Form.Group>
-          );
+          )
         })}
         <Row className={'mb-2'}>
           <Col>
             <Button
-              variant='success'
-              size='sm'
+              variant="success"
+              size="sm"
               onClick={() => props.stores.Clients.newClient()}
             >
               New +
@@ -101,12 +100,12 @@ function Clients(props) {
           </Col>
         </Row>
         <Row>
-          <Button variant='outline-secondary' size='sm' onClick={loadDefault}>
+          <Button variant="outline-secondary" size="sm" onClick={loadDefault}>
             load default
           </Button>
         </Row>
       </Form>
     </Container>
-  );
+  )
 }
-export default withStore(Clients);
+export default withStore(Clients)
