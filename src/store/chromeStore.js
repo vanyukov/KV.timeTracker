f38aa4043a642e7aaeb85055d786c18018ced751
Kirill Vanyukov <vanyukovkirill@gmail.com>
2021-11-25 11:27:53 +0300

    prettier

--- src/store/chromeStore.js
+++ src/store/chromeStore.js
@@ -1,103 +1,96 @@
-import { observable, computed, action } from 'mobx';
-import * as chrome from '~/api/chrome';
-import StoreClass from '~/store/StoreClass';
-import * as dateTime from '~/api/helpers/dateTime';
+import { observable, computed, action } from 'mobx'
+import * as chrome from '~/api/chrome'
+import StoreClass from '~/store/StoreClass'
+import * as dateTime from '~/api/helpers/dateTime'
 
 class chromeStore extends StoreClass {
-  @observable isExtensionMode;
-  @observable currentTab;
-  @observable isJiraTab;
-  @observable isUtzTab;
+  @observable isExtensionMode
+  @observable currentTab
+  @observable isJiraTab
+  @observable isUtzTab
 
   constructor(rootStore) {
-    super(rootStore);
-    this.isExtensionMode = chrome.isExtensionMode();
-    this.currentTab = null;
-    this.isJiraTab = false;
-    this.isUtzTab = false;
+    super(rootStore)
+    this.isExtensionMode = chrome.isExtensionMode()
+    this.currentTab = null
+    this.isJiraTab = false
+    this.isUtzTab = false
 
     if (this.isExtensionMode) {
-      this.setCurrentTab();
+      this.setCurrentTab()
     }
   }
 
   @action setCurrentTab = () => {
     chrome
       .getCurrentTab()
-      .then(result => (this.currentTab = result))
-      .then(result => {
+      .then((result) => (this.currentTab = result))
+      .then((result) => {
         this.isJiraTab = !!(
           this.currentTab.url.indexOf(
             this.rootStore.Settings.getSetting('jiraUrl')
           ) + 1
-        );
+        )
 
         this.isUtzTab = !!(
           this.currentTab.url.indexOf(
             this.rootStore.Settings.getSetting('utzUrl')
           ) + 1
-        );
-      });
-  };
+        )
+      })
+  }
 
   @action getFieldFromJira(field) {
     if (!this.isExtensionMode) {
-      return Promise.resolve(field);
+      return Promise.resolve(field)
     }
 
     if (field == 'ticket') {
-      return this.currentTab.url
-        .split('/')
-        .pop()
-        .split('?')
-        .shift();
+      return this.currentTab.url.split('/').pop().split('?').shift()
     } else if (field == 'title') {
       return chrome.runJS(
         this.currentTab,
         'document.querySelector("h1").textContent'
-      );
+      )
     } else if (field == 'branch') {
       return chrome.runJS(
         this.currentTab,
         'document.querySelector(".property-list .issue-link").textContent'
-      );
+      )
     } else if (field == 'epic') {
       return chrome
         .runJS(
           this.currentTab,
           "document.querySelector(\"[data-fieldtype='gh-epic-link'] a\").href.split('/').pop()"
         )
-        .then(result => {
+        .then((result) => {
           if (result) {
-            return Promise.resolve(result);
+            return Promise.resolve(result)
           }
 
-          return this.getEpicLinkId().then(epicLinkId => {
-            return this.getEpicLink(
-              epicLinkId,
-              this.getFieldFromJira('ticket')
-            );
-          });
+          return this.getEpicLinkId().then((epicLinkId) => {
+            return this.getEpicLink(epicLinkId, this.getFieldFromJira('ticket'))
+          })
         })
-        .catch(e => {
-          console.log(e);
-        });
+        .catch((e) => {
+          console.log(e)
+        })
     }
   }
 
   @action getFieldFromUtz(field) {
     if (!this.isExtensionMode) {
-      return Promise.resolve(field);
+      return Promise.resolve(field)
     }
     if (!this.isUtzTab) {
-      return Promise.resolve(null);
+      return Promise.resolve(null)
     }
 
     if (field == 'date') {
       return chrome.runJS(
         this.currentTab,
         'document.querySelector("#contentwrapper .heading").innerText.match(/\\d\\d.\\d\\d.\\d\\d\\d\\d/)'
-      );
+      )
     }
   }
 
@@ -107,19 +100,19 @@ class chromeStore extends StoreClass {
       {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/json;charset=UTF-8'
-        }
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
       }
     )
-      .then(data => data.json())
-      .then(data => {
-        var epicLink = data.find(item => item.name === 'Epic Link');
+      .then((data) => data.json())
+      .then((data) => {
+        var epicLink = data.find((item) => item.name === 'Epic Link')
 
         if (epicLink.id) {
-          return Promise.resolve(epicLink.id);
+          return Promise.resolve(epicLink.id)
         }
-        return Promise.reject();
-      });
+        return Promise.reject()
+      })
   }
 
   getEpicLink(epicLinkId, ticket) {
@@ -129,63 +122,60 @@ class chromeStore extends StoreClass {
       {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/json;charset=UTF-8'
-        }
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
       }
     )
-      .then(data => data.json())
-      .then(data => {
+      .then((data) => data.json())
+      .then((data) => {
         //Если есть эпик линк - отдадим его
         if (data.fields[epicLinkId]) {
-          return Promise.resolve(data.fields[epicLinkId]);
+          return Promise.resolve(data.fields[epicLinkId])
           //Если issue = эпик, отдадим его же ключ
         } else if (data.fields.issuetype.name === 'Epic') {
-          return Promise.resolve(data.key);
+          return Promise.resolve(data.key)
           //Если есть родитель, то пытаемся найти его.
         } else if (data.fields.parent) {
-          return this.getEpicLink(epicLinkId, data.fields.parent.key);
+          return this.getEpicLink(epicLinkId, data.fields.parent.key)
         } else {
-          return Promise.reject();
+          return Promise.reject()
         }
-      });
+      })
   }
 
   @action saveJira(track) {
-    const instance = this;
+    const instance = this
     function injectTimeLogField(query) {
       return instance.injectField(
         "document.getElementById('log-work-time-logged')",
         query
-      );
+      )
     }
 
     if (track.ticket != this.getFieldFromJira('ticket')) {
-      return Promise.reject('');
+      return Promise.reject('')
     }
-    chrome.runJS(
-      this.currentTab,
-      "document.getElementById('log-work').click()"
-    );
+    chrome.runJS(this.currentTab, "document.getElementById('log-work').click()")
 
     //Time
-    let trackTime = track.elapsedTime;
+    let trackTime = track.elapsedTime
     if (track.active) {
-      trackTime += new Date() - track.startTime;
+      trackTime += new Date() - track.startTime
     }
     if (track.overtime) {
-      trackTime *= 2;
+      trackTime *= 2
     }
-    const elapsedTime = dateTime.timeDiffSplitted(0, 0, trackTime);
+    const elapsedTime = dateTime.timeDiffSplitted(0, 0, trackTime)
     injectTimeLogField(
       "document.getElementById('log-work-time-logged').value = '" +
         elapsedTime.hours +
         'h ' +
         elapsedTime.minutes +
         "m'"
-    );
+    )
 
     //Date
-    const dateStart = dateTime.parseDate(track.date);
+    const dateStart = dateTime.parseDate(track.date)
     const time =
       dateStart.date +
       '/' +
@@ -193,15 +183,15 @@ class chromeStore extends StoreClass {
       '/' +
       dateTime.getFormat('YY hh:mm', track.date) +
       ' ' +
-      (dateStart.hour > 11 ? 'PM' : 'AM');
+      (dateStart.hour > 11 ? 'PM' : 'AM')
     injectTimeLogField(
       "document.getElementById('log-work-date-logged-date-picker').value = '" +
         time +
         "'"
-    );
+    )
 
     //Comment
-    setTimeout(function() {
+    setTimeout(function () {
       injectTimeLogField(
         "document.querySelector('iframe').contentWindow.document.querySelector('.mce-content-body').innerHTML = '<p>" +
           track.comment +
@@ -212,23 +202,23 @@ class chromeStore extends StoreClass {
           "document.querySelector('#log-work #comment').value = '" +
           track.comment +
           "'; "
-      );
-    }, 1000);
+      )
+    }, 1000)
   }
 
   injectField(checkedField, query) {
-    const instance = this;
-    return chrome.runJS(this.currentTab, checkedField).then(result => {
+    const instance = this
+    return chrome.runJS(this.currentTab, checkedField).then((result) => {
       if (result) {
-        return chrome.runJS(instance.currentTab, query);
+        return chrome.runJS(instance.currentTab, query)
       } else {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
-            resolve(instance.injectField(checkedField, query));
-          }, 100);
-        });
+            resolve(instance.injectField(checkedField, query))
+          }, 100)
+        })
       }
-    });
+    })
   }
 
   @action saveUTZ(track) {
@@ -236,19 +226,19 @@ class chromeStore extends StoreClass {
       track.startTime,
       track.active ? new Date() : 0,
       track.elapsedTime
-    );
+    )
 
     //open popup
     chrome
       .runJS(this.currentTab, "document.getElementsByTagName('iframe')[0]")
-      .then(result => {
+      .then((result) => {
         if (!result) {
           chrome.runJS(
             this.currentTab,
             "document.querySelector('#addRowLink').click()"
-          );
+          )
         }
-      });
+      })
 
     if (track.client) {
       //Клиент
@@ -257,25 +247,25 @@ class chromeStore extends StoreClass {
         "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#ReportRow_customer_id').value = " +
           track.client
       )
-        .then(result => {
+        .then((result) => {
           return chrome.runJS(
             this.currentTab,
             "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#ReportRow_customer_id').dispatchEvent(new Event('change'))"
-          );
+          )
         })
         //Код задачи
-        .then(result => {
+        .then((result) => {
           let id = this.rootStore.Clients.items.find(
-            item => item.id === track.client
-          )?.taskCode;
+            (item) => item.id === track.client
+          )?.taskCode
           //Код задачи для МТС
           if (track.client === '19') {
             if (track.ticket.match(/^IMO-/)) {
-              id = 1289;
+              id = 1289
             } else if (track.ticket.match(/^IMD-/)) {
-              id = 1268;
+              id = 1268
             } else if (track.ticket.match(/^IMSUPNEW-/)) {
-              id = 1258;
+              id = 1258
             }
           }
           if (id) {
@@ -287,20 +277,20 @@ class chromeStore extends StoreClass {
                     id +
                     "'"
                 )
-                .then(result => {
+                .then((result) => {
                   return chrome.runJS(
                     this.currentTab,
                     "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#ReportRow_task_code').dispatchEvent(new Event(\"change\"))"
-                  );
-                });
-            }, 1000);
+                  )
+                })
+            }, 1000)
           }
-        });
+        })
 
       // Задача по CR *
       const idCr = this.rootStore.Clients.items.find(
-        item => item.id === track.client
-      )?.taskIdCr;
+        (item) => item.id === track.client
+      )?.taskIdCr
       if (idCr) {
         setTimeout(() => {
           chrome
@@ -310,13 +300,13 @@ class chromeStore extends StoreClass {
                 idCr +
                 "'"
             )
-            .then(result => {
+            .then((result) => {
               return chrome.runJS(
                 this.currentTab,
                 "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#ReportRow_task_id_chzn').dispatchEvent(new Event(\"change\"))"
-              );
-            });
-        }, 1000);
+              )
+            })
+        }, 1000)
       }
     }
 
@@ -326,7 +316,7 @@ class chromeStore extends StoreClass {
       "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#ReportRow_issue').value = '" +
         track.ticket +
         "'"
-    );
+    )
 
     // Номер эпика
     chrome.runJS(
@@ -334,7 +324,7 @@ class chromeStore extends StoreClass {
       "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#ReportRow_epic_number').value = '" +
         track.epic +
         "'"
-    );
+    )
 
     // Время (ч)
     chrome.runJS(
@@ -345,7 +335,7 @@ class chromeStore extends StoreClass {
           ? '.' + Math.round((+elapsedTime.minutes * 100) / 60)
           : '') +
         "'"
-    );
+    )
 
     //Комментарий
     chrome.runJS(
@@ -353,23 +343,23 @@ class chromeStore extends StoreClass {
       "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#ReportRow_comment').value = '" +
         track.comment +
         "'"
-    );
+    )
     chrome.runJS(
       this.currentTab,
       "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#comments').text = '" +
         track.comment +
         "'"
-    );
+    )
     chrome.runJS(
       this.currentTab,
       "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('.delete_comment').style.cssText = ''"
-    );
+    )
     chrome.runJS(
       this.currentTab,
       "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('.editable-input .fancy_textarea').value = '" +
         track.comment +
         "'"
-    );
+    )
 
     //Внеурочка
     chrome.runJS(
@@ -377,21 +367,21 @@ class chromeStore extends StoreClass {
       "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#ReportRow_vu').checked = " +
         track.overtime +
         ';'
-    );
+    )
 
     // Тип работ
     const typeUtz = this.rootStore.UtzJobTypes.items.find(
-      item => track.typeUTZ == item.key
-    );
+      (item) => track.typeUTZ == item.key
+    )
     setTimeout(() => {
       chrome.runJS(
         this.currentTab,
         "document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('#ReportRow_work_type_1').value = '" +
           typeUtz.type +
           "'"
-      );
-    }, 1000);
+      )
+    }, 1000)
   }
 }
 
-export default chromeStore;
+export default chromeStore
