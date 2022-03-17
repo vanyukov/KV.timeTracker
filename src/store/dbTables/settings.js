31ff128b17c7df77136087ec95a37c88adce9ac5
Kirill Vanyukov <vanyukovkirill@gmail.com>
2022-03-17 16:42:59 +0300

    prettier

--- src/store/dbTables/settings.js
+++ src/store/dbTables/settings.js
@@ -1,61 +1,63 @@
-import { observable, computed, action } from "mobx";
-import StoreClass from "../StoreClass";
+import { observable, computed, action } from 'mobx'
+import StoreClass from '../StoreClass'
 
 export default class Settings extends StoreClass {
-  @observable items;
+  @observable items
 
   constructor(rootStore) {
-    super(rootStore);
-    this.table = "settings";
-    this.items = [];
+    super(rootStore)
+    this.table = 'settings'
+    this.items = []
     this.defaultSettings = [
       {
-        name: "jiraUrl",
-        value: "https://jira.goods.ru/"
+        name: 'jiraUrl',
+        value: 'https://jira.goods.ru/',
       },
       {
-        name: "utzUrl",
-        value: "http://timesheet.services.lenvendo.ru"
+        name: 'utzUrl',
+        value: 'http://timesheet.services.lenvendo.ru',
       },
       {
-        name: "gitRepositoryUrl",
-        value: "https://git.shop.mts.ru/shop/backend/site_bitrix"
-      }
-    ];
+        name: 'gitRepositoryUrl',
+        value: 'https://git.shop.mts.ru/shop/backend/site_bitrix',
+      },
+    ]
   }
 
   @action getSetting(name) {
-    const setting = this.items.find(item => item.name == name);
+    const setting = this.items.find((item) => item.name == name)
     if (setting) {
-      return setting.value;
+      return setting.value
     }
 
-    const defaultSetting = this.defaultSettings.find(item => item.name == name);
+    const defaultSetting = this.defaultSettings.find(
+      (item) => item.name == name
+    )
     if (defaultSetting) {
-      this.rootStore.dbStore.saveTableRow(this.table, defaultSetting);
-      return defaultSetting.value;
+      this.rootStore.dbStore.saveTableRow(this.table, defaultSetting)
+      return defaultSetting.value
     }
   }
 
   @action loadDefault() {
-    this.items.length = 0;
-    this.defaultSettings.forEach(item => {
-      this.rootStore.dbStore.saveTableRow(this.table, item);
-      this.items.push(item);
-    });
+    this.items.length = 0
+    this.defaultSettings.forEach((item) => {
+      this.rootStore.dbStore.saveTableRow(this.table, item)
+      this.items.push(item)
+    })
   }
 
   @action loadSettings() {
-    this.items = [];
-    this.rootStore.dbStore.loadTableRows(this.table).then(data => {
+    this.items = []
+    this.rootStore.dbStore.loadTableRows(this.table).then((data) => {
       if (!Array.isArray(data)) {
-        return null;
+        return null
       }
-      data.forEach(item => this.items.push(item));
-    });
+      data.forEach((item) => this.items.push(item))
+    })
   }
 
   @action changeSetting(name, value) {
-    this.items.find(item => item.name == name).value = value;
+    this.items.find((item) => item.name == name).value = value
   }
 }
