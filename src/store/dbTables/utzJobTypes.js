68c20f81cd20ed89b292e113aa4cb38fa886c776
Kirill Vanyukov <vanyukovkirill@gmail.com>
2022-03-28 18:12:59 +0300

    prettier

--- src/store/dbTables/utzJobTypes.js
+++ src/store/dbTables/utzJobTypes.js
@@ -1,71 +1,71 @@
-import { observable, computed, action } from 'mobx';
-import StoreClass from '../StoreClass';
-import utzJobTypes from '~/api/db/utzJobTypes';
+import { observable, computed, action } from 'mobx'
+import StoreClass from '../StoreClass'
+import utzJobTypes from '~/api/db/utzJobTypes'
 
 export default class UtzJobTypes extends StoreClass {
-  @observable items = [];
+  @observable items = []
 
   constructor(rootStore) {
-    super(rootStore);
-    this.table = 'utzJobTypes';
-    this.items = [];
+    super(rootStore)
+    this.table = 'utzJobTypes'
+    this.items = []
     this.defaultUtzJobTypes = [
       {
         id: 1,
-        type: 'Разработка проектных решений'
+        type: 'Разработка проектных решений',
       },
       {
         id: 2,
-        type: 'Консультация специалистов по задачам, помощь в решении задач'
+        type: 'Консультация специалистов по задачам, помощь в решении задач',
       },
       {
         id: 3,
-        type: 'Отладка функционала в рамках этапа тестирования и отладки задач'
+        type: 'Отладка функционала в рамках этапа тестирования и отладки задач',
       },
       {
         id: 4,
-        type: 'Решение задач в рамках Поддержки во внеурочное время'
-      }
-    ];
+        type: 'Решение задач в рамках Поддержки во внеурочное время',
+      },
+    ]
   }
 
   @action loadDefault() {
-    this.defaultUtzJobTypes.forEach(item => {
-      this.rootStore.dbStore.saveTableRow(this.table, item);
-      this.items.push(item);
-    });
+    this.defaultUtzJobTypes.forEach((item) => {
+      this.rootStore.dbStore.saveTableRow(this.table, item)
+      this.items.push(item)
+    })
   }
 
   @action newUtzJobType() {
     this.rootStore.dbStore
       .saveTableRow(this.table, utzJobTypes.getNew())
-      .then(res => {
-        const newItem = utzJobTypes.getNew();
-        newItem.key = res;
-        this.items.push(newItem);
-      });
+      .then((res) => {
+        const newItem = utzJobTypes.getNew()
+        newItem.key = res
+        this.items.push(newItem)
+      })
   }
 
   @action loadUtzJobTypes() {
-    this.rootStore.dbStore.loadTableRowsWithKeys(this.table).then(data => {
+    this.rootStore.dbStore.loadTableRowsWithKeys(this.table).then((data) => {
       if (Array.isArray(data) && data.length) {
-        data.forEach(item => this.items.push(item));
+        data.forEach((item) => this.items.push(item))
       } else {
-        this.loadDefault();
+        this.loadDefault()
       }
-    });
+    })
   }
 
   @action changeUtzJobType(key, field, value) {
-    this.items.find(item => item.key == key)[field] = value;
+    this.items.find((item) => item.key == key)[field] = value
   }
 
   @action saveUtzJobType(UtzJobType) {
-    this.rootStore.dbStore.saveTableRow(this.table, UtzJobType, UtzJobType.key);
+    this.rootStore.dbStore.saveTableRow(this.table, UtzJobType, UtzJobType.key)
   }
 
   @action deleteUtzJobType(UtzJobType) {
-    this.items = this.items.filter(item => UtzJobType.key != item.key);
-    this.rootStore.dbStore.deleteTableRow(this.table, UtzJobType.key);
+    this.items = this.items.filter((item) => UtzJobType.key != item.key)
+    this.rootStore.dbStore.deleteTableRow(this.table, UtzJobType.key)
   }
 }
