4a8e8e0c187c21ae778173d60e8ada57397fc940
Kirill Vanyukov <vanyukovkirill@gmail.com>
2022-09-14 11:58:20 +0300

    jiraUrl = lenvendo

--- src/store/dbTables/settings.js
+++ src/store/dbTables/settings.js
@@ -9,7 +9,7 @@ export default class Settings extends StoreClass {
       {
         id: 1,
         name: 'jiraUrl',
-        value: 'https://jira.goods.ru/',
+        value: 'https://lenvendo-ru.atlassian.net/',
       },
       {
         id: 2,
