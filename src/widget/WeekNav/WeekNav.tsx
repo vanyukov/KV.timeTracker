0251bab7564be188a680f37a779ae887434fe57e
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-08-16 16:12:13 +0530

    add WeekNav base functions

---
+++ src/widget/WeekNav/WeekNav.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames"
+import dayjs from "dayjs"
+import { Link } from "ui"
+import { weekDays } from "./weekDays"
+import style from "./WeekNav.module.scss"
+
+export type WeekNavProps = {
+  className?: string
+}
+
+export function WeekNav({ className }: WeekNavProps) {
+  const startWeek = dayjs().startOf("week")
+
+  return (
+    <div className={classNames(className, style.wrap)}>
+      {weekDays.map(item => {
+        const currentDay = startWeek.add(item.number, "day")
+        return (
+          <Link
+            to={`/day/${currentDay.year()}/${currentDay.month()}/${currentDay.date()}`}
+          >
+            {item.title}
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+WeekNav.defaultProps = {
+  className: "",
+}
---
+++ src/widget/WeekNav/weekDays.ts
@@ -0,0 +1,30 @@
+export const weekDays = [
+  {
+    number: 1,
+    title: "M",
+  },
+  {
+    number: 2,
+    title: "T",
+  },
+  {
+    number: 3,
+    title: "W",
+  },
+  {
+    number: 4,
+    title: "T",
+  },
+  {
+    number: 5,
+    title: "F",
+  },
+  {
+    number: 6,
+    title: "S",
+  },
+  {
+    number: 7,
+    title: "S",
+  },
+]
