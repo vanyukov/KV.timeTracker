fea3711fb5ee3d840141859681da93b88298802f
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-08-18 21:27:30 +0530

    update storybook

--- scripts/createComponent/templates/storyTemplate.js
+++ scripts/createComponent/templates/storyTemplate.js
@@ -9,6 +9,7 @@ export default meta
 type Story = StoryObj<typeof ${componentName}Component>
 
 export const ${componentName}: Story = {
+  args: {},
   render: props => (<${componentName}Component {...props} />),
 }
 `;
