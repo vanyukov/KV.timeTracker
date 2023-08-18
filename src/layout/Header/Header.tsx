f30c071e55690e0fb84a11093f076b8c29b990b1
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-08-18 21:27:53 +0530

    update Header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,11 +1,11 @@
-import classNames from "classnames"
-import { Button, Link } from "ui"
+import dayjs from "dayjs"
+import { Button, Link, Typography } from "ui"
 import style from "./Header.module.scss"
 
 export function Header() {
   return (
     <div className="container">
-      <header className={classNames("flex pt12 pb12", style.header)}>
+      <header className="flex g8 pt12 pb12">
         <Link href="/" className={style.logoLink}>
           <img
             src="/img/logo.png"
@@ -15,6 +15,9 @@ export function Header() {
           />
         </Link>
         <Button color="success">Add Track</Button>
+        <Typography variant="h5" component="p">
+          {dayjs().format("DD MMMM YYYY, dddd")}
+        </Typography>
       </header>
     </div>
   )
