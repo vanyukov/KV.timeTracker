cd8b287ae09ace06a452549b6c52201c7a35add2
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-08-18 23:10:28 +0530

    WeekNav

--- src/widget/WeekNav/WeekNav.tsx
+++ src/widget/WeekNav/WeekNav.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames"
 import dayjs from "dayjs"
-import { Link } from "ui"
+import { Link, Typography } from "ui"
+import { useActiveDay } from "common/helpers"
 import { weekDays } from "./weekDays"
 import style from "./WeekNav.module.scss"
 
@@ -10,16 +11,39 @@ export type WeekNavProps = {
 
 export function WeekNav({ className }: WeekNavProps) {
   const startWeek = dayjs().startOf("week")
+  const activeDay = useActiveDay()
+  const today = dayjs().format("D M YYYY")
 
   return (
-    <div className={classNames(className, style.wrap)}>
+    <div className={classNames("flex g12", className)}>
       {weekDays.map(item => {
-        const currentDay = startWeek.add(item.number, "day")
+        const currentDay = startWeek.add(item, "day")
+
+        if (activeDay === currentDay.format("D M YYYY")) {
+          return (
+            <Typography
+              variant="button"
+              className={classNames("flex", style.link)}
+            >
+              <span>{currentDay.format("ddd")}</span>
+              <span>{currentDay.format("DD")}</span>
+            </Typography>
+          )
+        }
+
+        const link = today === currentDay.format("D M YYYY")
+          ? "/"
+          : `/day/${currentDay.format("YYYY/M/D")}`
+
         return (
           <Link
-            href={`/day/${currentDay.year()}/${currentDay.month()}/${currentDay.date()}`}
+            key={item}
+            href={link}
+            variant="button"
+            className={classNames("flex", style.link)}
           >
-            {item.title}
+            <span>{currentDay.format("ddd")}</span>
+            <span>{currentDay.format("DD")}</span>
           </Link>
         )
       })}
