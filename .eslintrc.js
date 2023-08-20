da15acff215b762cbe3af3aa7d9ab8bb1c2c652e
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-08-20 21:45:08 +0530

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     "@typescript-eslint/strict-boolean-expressions": "off",
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "@typescript-eslint/no-unused-vars": "warn",
     "react/react-in-jsx-scope": "off",
     "react/jsx-props-no-spreading": "off",
     "arrow-parens": "off",
