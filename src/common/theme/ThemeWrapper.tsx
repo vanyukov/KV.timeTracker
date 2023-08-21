3cbece32fb25c59c94280dbb7b3b443822762de4
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-08-21 14:40:06 +0530

    shrink fonts, only latin

--- src/common/theme/ThemeWrapper.tsx
+++ src/common/theme/ThemeWrapper.tsx
@@ -1,8 +1,8 @@
 import { CssBaseline, ThemeProvider } from "@mui/material"
-import "@fontsource/roboto/300.css"
-import "@fontsource/roboto/400.css"
-import "@fontsource/roboto/500.css"
-import "@fontsource/roboto/700.css"
+import "@fontsource/roboto/latin-300.css"
+import "@fontsource/roboto/latin-400.css"
+import "@fontsource/roboto/latin-500.css"
+import "@fontsource/roboto/latin-700.css"
 import { theme } from "./theme"
 
 export function ThemeWrapper({ children }: { children: React.ReactNode }) {
