42bec6cc867351fdd97dc922a78ea0d22b05c28f
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-10-02 21:04:39 +0530

    KVTT-24. add SpeedDial+SpeedDialAction

--- src/ui/SpeedDialAction/SpeedDialAction.tsx
+++ src/ui/SpeedDialAction/SpeedDialAction.tsx
@@ -2,7 +2,7 @@ import * as MuiSpeedDialAction from "@mui/material/SpeedDialAction"
 
 export type SpeedDialActionProps = MuiSpeedDialAction.SpeedDialActionProps
 
-export function SpeedDialAction({ children, ...props }: SpeedDialActionProps) {
+export function SpeedDialAction(props: SpeedDialActionProps) {
   // eslint-disable-next-line react/jsx-pascal-case
-  return <MuiSpeedDialAction.default {...props}>{children}</MuiSpeedDialAction.default>
+  return <MuiSpeedDialAction.default {...props} />
 }
