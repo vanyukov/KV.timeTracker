e3d979a5fd55b45395eb0d75ac17919179615ef7
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-10-03 23:25:56 +0530

    KVTT-14. dayjs add plugins: utc, timezone

--- src/common/dateTime/dateLib.ts
+++ src/common/dateTime/dateLib.ts
@@ -1,9 +1,14 @@
 import dayjs, { type Dayjs } from "dayjs"
 import updateLocale from "dayjs/plugin/updateLocale"
+import utc from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone"
 
 dayjs.extend(updateLocale)
 dayjs.updateLocale("en", {
   weekStart: 1,
 })
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
 export const dateLib = dayjs
 export type TdateLib = Dayjs
