105970eac5e4d17504b4957d3aaef27168f3c25f
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-10-11 21:00:47 +0530

    KVTT-50. track table: active row color

---
+++ src/common/theme/componentsMui/MuiTableRow.ts
@@ -0,0 +1,9 @@
+export const MuiTableRow = {
+  styleOverrides: {
+    root: {
+      "&.Mui-selected": {
+        backgroundColor: "color-mix(in srgb, var(--mui-palette-success-main) 20%, white)",
+      },
+    },
+  },
+}
