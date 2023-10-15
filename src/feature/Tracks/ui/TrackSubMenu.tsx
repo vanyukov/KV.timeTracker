e45ff0ef295ab42439633addcb1666a96df4bc5d
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-10-15 17:09:02 +0530

    KVTT-38. Refactoring TrackSubMenu -> MenuComponent

--- src/feature/Tracks/ui/TrackSubMenu.tsx
+++ src/feature/Tracks/ui/TrackSubMenu.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react"
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz"
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever"
-import { Button, Menu, MenuItem } from "ui"
+import { MenuComponent } from "ui"
 import { useAppDispatch } from "store"
 import { tracksDeleteItem } from "../redux"
 
@@ -11,32 +10,18 @@ export type TrackSubMenuProps = {
 }
 
 export function TrackSubMenu({ id, onDelete = () => {} }: TrackSubMenuProps) {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
   const dispatch = useAppDispatch()
-
   const handleDelete = () => {
     void dispatch(tracksDeleteItem(id))
-    handleClose()
     onDelete()
   }
-  return (
-    <div>
-      <Button onClick={handleClick} variant="text" color="secondary">
-        <MoreHorizIcon />
-      </Button>
-      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-        <MenuItem onClick={handleDelete}>
-          <DeleteForeverIcon />
-        </MenuItem>
-      </Menu>
-    </div>
-  )
+
+  const menuItems = [
+    {
+      id: 1,
+      title: <DeleteForeverIcon />,
+      fn: handleDelete,
+    },
+  ]
+  return <MenuComponent title={<MoreHorizIcon />} items={menuItems} />
 }
