2049a8e9f19a78d4e9d3c30029ccce98239977ba
Kirill Vanyukov <vanyukovkirill@gmail.com>
2024-07-06 23:32:49 +0300

    menuItems id

--- src/layout/Header/menuItems.tsx
+++ src/layout/Header/menuItems.tsx
@@ -5,5 +5,5 @@ export const menuItems = [
   { title: <Link href="/reports">Reports</Link>, id: 1 },
   { title: <Link href="/clients">Clients</Link>, id: 2 },
   { title: <Link href="/projects">Projects</Link>, id: 3 },
-  { title: <BtnLogout />, id: 3 },
+  { title: <BtnLogout />, id: 4 },
 ]
